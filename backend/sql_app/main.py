3ef6eb82469012bb0fc20f31c8ee7a0a708f5eb1
Ibuki-Narukawa <ibuki0212job@gmail.com>
2022-06-15 17:12:43 +0900

    add CORS policy

--- backend/sql_app/main.py
+++ backend/sql_app/main.py
@@ -5,9 +5,19 @@ from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
+from fastapi.middleware.cors import CORSMiddleware
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 def get_db():
   db = SessionLocal()
